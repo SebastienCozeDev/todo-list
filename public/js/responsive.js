cd48dbf4b2e04641b40d2f7e55b9509098e7d0b8
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-10 15:11:31 +0100

    Mise à jour pour le responsive

--- public/js/responsive.js
+++ public/js/responsive.js
@@ -1,6 +1,7 @@
 const contentForDesktop = document.getElementById("responsive-only-desktop");
+const contentForSmartphone = document.getElementById("responsive-only-smartphone");
 if (window.innerWidth > 1024) {
-    contentForDesktop.style.display = "block";
+    contentForSmartphone.style.display = "none";
 } else {
     contentForDesktop.style.display = "none";
 }
